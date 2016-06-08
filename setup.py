f86d3884054f96b47ce799093c3fa314aa52fc96
Michael Doronin <warrior2031@gmail.com>
2016-06-08 10:09:17 +0300

    setup.py have been added

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='sqlalchemy-compilers',
+    version='',
+    install_requires=[
+        'sqlalchemy',
+        'pytest',
+    ],
+    py_modules=['compilers'],
+    url='',
+    license='MIT',
+    author='michael',
+    author_email='warrior2031@mail.ru',
+    description='sqlalchemy based models for ISS'
+)
