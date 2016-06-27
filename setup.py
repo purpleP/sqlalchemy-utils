135fded5c4a96049409e08381dc0545d11791a64
Vadim Gukhman <vadim.gukhman@moex.com>
2016-06-27 16:30:45 +0300

    * from modules to packages
    Signed-off-by: Vadim Gukhman <vadim.gukhman@moex.com>

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from distutils.core import setup
 
 setup(
-    name='sqlalchemy-compilers',
+    name='sqlalchemy-utils',
     version='',
     install_requires=[
         'sqlalchemy',
         'pytest',
     ],
-    py_modules=['compilers'],
+    packages=['sqlalchemy_utils'],
     url='',
     license='MIT',
     author='michael',
     author_email='warrior2031@mail.ru',
-    description='sqlalchemy based models for ISS'
+    description='sqlalchemy utils'
 )
